ce4a385c7b8657c68014e3f6fb0bef527a513969
Jorge R. <jorge@programmer.net>
2023-11-07 16:44:54 -0700

    add BudgetControl component & styles

---
+++ src/components/BudgetControl.tsx
@@ -0,0 +1,22 @@
+
+
+
+
+export const BudgetControl = () => {
+  return (
+    <div className="container container-budget two-columns shadow">
+      Control de presupuesto
+
+      <div>
+        Gráfica
+      </div>
+
+      <div className="content-budget">
+        <p>
+          <span>Presupuesto: </span> $500
+        </p>
+      </div>
+
+    </div>
+  )
+}
