28ddb54e4c2b0fa224384d8be53578cc58fe2a93
Jorge R. <jorge@programmer.net>
2023-11-13 12:51:08 -0700

    add expenses form to modal component

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,5 +1,12 @@
 
 
+import {
+  ENTERTAINMENT_CATEGORY,
+  FOOD_CATEGORY,
+  HEALTH_CATEGORY,
+  HOUSING_CATEGORY,
+  TRANSPORTATION_CATEGORY,
+} from '../constants/expensesTypes';
 import { ModalProps } from '../interfaces/interfaces';
 import { Close } from './Icons';
 
@@ -31,6 +38,53 @@ export const Modal = ( { setModal, animateModal, setAnimateModal }: ModalProps )
       >
 
         <legend>Nuevo Gasto</legend>
+
+        <div className="field">
+
+          <label htmlFor="name">Nombre del Gasto</label>
+
+          <input 
+            type="text" 
+            id="name" 
+            placeholder='Añade el nombre del gasto'
+            // name="name" i
+          />
+          
+        </div>
+
+        <div className="field">
+
+          <label htmlFor="amount">Cantidad</label>
+
+          <input 
+            type="number" 
+            id="amount" 
+            placeholder='Añade la cantidad del gasto'
+            // name="name" i
+          />
+
+        </div>
+
+        <div className="field">
+
+          <label htmlFor="category">Tipo de gasto</label>
+
+          <select 
+            id="category"
+            
+          >
+            <option value="">-- Seleccionar --</option>
+            <option value={ FOOD_CATEGORY }>Comida</option>
+            <option value={ TRANSPORTATION_CATEGORY }>Transporte</option>
+            <option value={ ENTERTAINMENT_CATEGORY }>Entretenimiento</option>
+            <option value={ HOUSING_CATEGORY }>Vivienda</option>
+            <option value={ HEALTH_CATEGORY }>Salud</option>
+
+          </select>
+
+        </div>
+
+        <input type="submit" value="Añadir Gasto" />
 
       </form>
 
---
+++ src/constants/expensesTypes.ts
@@ -0,0 +1,8 @@
+
+
+// constants for types of expenses
+export const FOOD_CATEGORY = "food";
+export const TRANSPORTATION_CATEGORY = "transportation";
+export const ENTERTAINMENT_CATEGORY = "entertainment";
+export const HOUSING_CATEGORY = "housing";
+export const HEALTH_CATEGORY = "health";
